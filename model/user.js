641279bf2cea3adae446a307659595f09a09be3e
Davinder <davibaweja@gmail.com>
2021-10-24 10:09:04 +0100

    added the userSchema for the database

---
+++ model/user.js
@@ -0,0 +1,31 @@
+// In this file we will design the schema of the user
+const mongoose = require("mongoose");
+
+
+const userSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        min:3,
+        max:255
+    },
+    email:{
+        type:String,
+        required:true,
+        min:6,
+        max:255
+    },
+    password:{
+        type:String,
+        required:true,
+        min:6,
+        max:1024
+    },
+    date:{
+        type:Date,
+        default:Date.now
+    }
+});
+
+
+module.exports=mongoose.model("user",userSchema);
