9076558108ebf089340d7ace5c4d6a16ea8a7026
Davinder <davibaweja@gmail.com>
2021-10-25 17:42:18 +0100

    installed Joi for validation and setup is done

--- routes/auth.js
+++ routes/auth.js
@@ -3,22 +3,47 @@ const User=require("../model/User");
 
 
 
+// Steps in the video did not work as they were old
+// see syntax below how to define the Joi object and how to validate
+
+
+//Validation 
+const Joi = require('@hapi/joi');
+
+const schema =Joi.object({
+    name:Joi.string()
+        .min(6)
+        .required(),
+    email:Joi.string()
+        .min(6)
+        .required()
+        .email(),
+    password:Joi.string()
+        .min(6)
+        .required()
+});
+
+
 
 router.post("/register", async(req,res)=>{
-    const newUser=new User({
-        name:req.body.name,
-        email:req.body.email,
-        password:req.body.password
-    });
+    //Lets validate the inputs before making a new user :)
+    const validation=schema.validate(req.body)
+    res.send(validation)
 
-    try{
-        const savedUser=await newUser.save(); // This line writes into the database
-        res.send(savedUser);
+    // const newUser=new User({
+    //     name:req.body.name,
+    //     email:req.body.email,
+    //     password:req.body.password
+    // });
 
-    }catch(err)
-    {
-        res.status(400).send(err)
-    }
+    // try{
+    //     const savedUser=await newUser.save(); // This line writes into the database
+    //     res.send(savedUser);
+
+    // }catch(err)
+    // {
+    //     res.status(400).send(err)
+    // }
 
 });
 
